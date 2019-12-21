73a3f4ecb70f2eacbc4c908445a0d76c2ef78732
Aleksei Bingham <asb9189@rit.edu>
2019-12-21 01:56:16 -0500

    Added more functionality to the Graph class such as modifying its constructor for a start and finish node

--- src/Main/Graph.java
+++ src/Main/Graph.java
@@ -1,15 +1,39 @@
 package Main;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 public class Graph {
 
+    private MyNode start;
+    private MyNode finish;
     private int vertices;
     private ArrayList<MyNode> nodes;
 
-    public Graph(int vertices) {
+    public Graph(MyNode start, MyNode finish, int vertices, ArrayList<MyNode> nodes) {
+        this.start = start;
+        this.finish = finish;
         this.vertices = vertices;
-        nodes = new ArrayList<>();
+        this.nodes = nodes;
+    }
+
+    public String toString() {
+        String result = "";
+        for (MyNode node : nodes) {
+            result += node.toString() + "\n";
+        }
+        return result;
+    }
+
+    public MyNode getStart() {
+        return start;
+    }
+
+    public MyNode getFinish() {
+        return finish;
+    }
+
+    public int getSize() {
+        return vertices;
     }
 
 }
