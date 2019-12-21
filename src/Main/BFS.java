e80365e724acb4bbd5be608f9df77b019c319fdb
Aleksei Bingham <asb9189@rit.edu>
2019-12-21 12:27:24 -0500

    Implemented the path construction and blue coloring of buttons path

--- src/Main/BFS.java
+++ src/Main/BFS.java
@@ -1,5 +1,9 @@
 package Main;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -14,9 +18,14 @@ public class BFS extends Thread implements Runnable {
     private MyNode finish;
     private List<MyNode> queue;
     private Map<MyNode, MyNode> predecessors;
+    private boolean solution;
+    private GridPane gridPane;
+    private VBox buttons;
 
-    public BFS(Graph graph) {
+    public BFS(Graph graph, GridPane gridPane, VBox vbox) {
         this.graph = graph;
+        this.gridPane = gridPane;
+        this.buttons = vbox;
         start = graph.getStart();
         finish = graph.getFinish();
         queue = new LinkedList<MyNode>();
@@ -26,6 +35,9 @@ public class BFS extends Thread implements Runnable {
 
     @Override
     public void run() {
+
+        //Start off by saying we don't have a solution
+        solution = false;
 
         //add the start node into the queue
         queue.add(start);
@@ -37,8 +49,8 @@ public class BFS extends Thread implements Runnable {
         while (!queue.isEmpty()) {
 
             MyNode currentNode = queue.remove(0);
-            System.out.println("current node: " + currentNode);
             if (currentNode == finish) {
+                solution = true;
                 constructPath();
                 break;
             }
@@ -54,8 +66,13 @@ public class BFS extends Thread implements Runnable {
 
             }
         }
+
         //if the queue runs empty no path could be found
-        System.out.println("No Path");
+
+        if (!solution) {
+            System.out.println("No Path");
+        }
+
 
     }
 
@@ -71,9 +88,26 @@ public class BFS extends Thread implements Runnable {
             path.add(start.getName());
         }
 
-        for (String str : path) {
-            System.out.println(str);
+        ArrayList<String> newPath = new ArrayList<>();
+        int cost = 0;
+        while (!path.isEmpty()) {
+            newPath.add(path.remove(path.size() - 1));
+            cost++;
         }
+
+
+
+        for (String element : newPath) {
+            MyButton b = (MyButton) gridPane.getChildren().get(Integer.parseInt(element) - 1);
+            b.setStyle("-fx-background-color: #3342FF; ");
+        }
+
+        gridPane.setDisable(true);
+        buttons.getChildren().get(0).setDisable(true);
+        buttons.getChildren().get(1).setDisable(true);
+
+        System.out.println("Path from Start -> Finish: " + newPath);
+        System.out.println("Cost: " + (cost - 1));
 
     }
 
